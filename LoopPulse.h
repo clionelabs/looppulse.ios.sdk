968824449f1c8140876b9d6072718a425743bd22
Thomas Pun <thomas@pun.io>
2014-05-02 11:41:04 +0800

    added empty LoopPulse .h and .m

---
+++ LoopPulse.h
@@ -0,0 +1,13 @@
+//
+//  LoopPulse.h
+//  LightHouse
+//
+//  Created by Thomas Pun on 5/2/14.
+//  Copyright (c) 2014 Clione Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoopPulse : NSObject
+
+@end
---
+++ LoopPulse.m
@@ -0,0 +1,13 @@
+//
+//  LoopPulse.m
+//  LightHouse
+//
+//  Created by Thomas Pun on 5/2/14.
+//  Copyright (c) 2014 Clione Labs. All rights reserved.
+//
+
+#import "LoopPulse.h"
+
+@implementation LoopPulse
+
+@end
